29edb5d97de919ec907101c36741ba4ac989a8bc
Aaron Faanes <dafrito@gmail.com>
2017-01-12 03:22:30 -0600

    getVerts

--- src/gl.js
+++ src/gl.js
@@ -14,6 +14,47 @@ function parsegraph_generateRectangleVertices(x, y, w, h) {
     ];
 }
 
+function parsegraph_getVerts(width,length,height)
+{
+    return [
+        // Front
+        [-width, length, height], // v0
+        [ width, length, height], // v1
+        [ width, length,-height], // v2
+        [-width, length,-height], // v3
+
+        // Back
+        [ width,-length, height], // v4
+        [-width,-length, height], // v5
+        [-width,-length,-height], // v6
+        [ width,-length,-height], // v7
+
+        // Left
+        [width, length, height], // v1
+        [width,-length, height], // v4
+        [width,-length,-height], // v7
+        [width, length,-height], // v2
+
+        // Right
+        [-width,-length, height], // v5
+        [-width, length, height], // v0
+        [-width, length,-height], // v3
+        [-width,-length,-height], // v6
+
+        // Top
+        [ width, length, height], // v1
+        [-width, length, height], // v0
+        [-width,-length, height], // v5
+        [ width,-length, height], // v4
+
+        // Bottom
+        [ width,-length,-height], // v7
+        [-width,-length,-height], // v6
+        [-width, length,-height], // v3
+        [ width, length,-height] //v2
+    ];
+}
+
 function parsegraph_generateRectangleTexcoords() {
     return [
         0, 0,
