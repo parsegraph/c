26bf715e9c57ead3dac2eabe5da71de724f5d332
Aaron Faanes <dafrito@gmail.com>
2017-01-18 00:31:53 -0600

    Moved functions in constants.js into initialize.js

--- src/graph/initialize.js
+++ src/graph/initialize.js
@@ -190,3 +190,34 @@ function parsegraph_initialize(mathMode) {
     parsegraph_EXTENT_BORDER_ROUNDEDNESS = parsegraph_BUD_RADIUS;
     parsegraph_EXTENT_BORDER_THICKNESS = parsegraph_BUD_RADIUS;
 }
+
+function parsegraph_style(type)
+{
+    type = parsegraph_readNodeType(type);
+
+    switch(type) {
+    case parsegraph_BUD:
+    {
+        return parsegraph_BUD_STYLE;
+    }
+    case parsegraph_SLOT:
+    {
+        return parsegraph_SLOT_STYLE;
+    }
+    case parsegraph_BLOCK:
+    {
+        return parsegraph_BLOCK_STYLE;
+    }
+    case parsegraph_SLIDER:
+    {
+        return parsegraph_SLIDER_STYLE;
+    }
+    case parsegraph_SCENE:
+    {
+        return parsegraph_SCENE_STYLE;
+    }
+    case parsegraph_NULL_NODE_TYPE:
+    default:
+        return null;
+    }
+};
