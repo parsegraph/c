2180c845c41fc355e08e2d3906c2a35571998938
Aaron Faanes <dafrito@gmail.com>
2017-02-20 17:49:01 -0600

    Disable quiet during inotifywait

--- autodeploy.sh
+++ autodeploy.sh
@@ -4,6 +4,6 @@ TARGET=${*:-$HOME/public_html/parsegraph}
 
 mkdir -p $TARGET
 
-while inotifywait -e modify -q -r src www; do
+while inotifywait -e modify -r src www; do
     make && make install && /bin/cp -vuf *.js www/* $TARGET
 done
