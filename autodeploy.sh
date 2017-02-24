e1476bd4f4d481005fe9dbdb20d9a5e18d741341
Aaron Faanes <dafrito@gmail.com>
2017-02-23 18:17:13 -0600

    Deploy at least once when running deploy

--- autodeploy.sh
+++ autodeploy.sh
@@ -4,6 +4,11 @@ TARGET=${*:-$HOME/public_html/parsegraph}
 
 mkdir -p $TARGET
 
-while inotifywait -e modify -r src www; do
+deploy() {
     make && make install && /bin/cp -vuf *.js www/* $TARGET
+}
+
+deploy
+while inotifywait -e modify -r src www; do
+    deploy
 done
