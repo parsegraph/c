726b2a440a944535f334fb9d28cea39324b65d5b
Aaron Faanes <dafrito@gmail.com>
2017-06-08 23:46:37 -0500

    Avoid uploading temporary files

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ TARGET=${*:-$HOME/src/parsegraph/server/public_html}
 
 mkdir -p $TARGET
 
-make && make install && /bin/cp -vuf *.js www/* $TARGET
+make && make install && /bin/cp -vuf *.js www/*.html $TARGET
