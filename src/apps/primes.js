3b48931ffec361dd45cb04aa2bdab5352781f57e
Aaron Faanes <dafrito@gmail.com>
2019-08-06 19:43:20 -0500

    Let a column be highlighted

--- src/apps/primes.js
+++ src/apps/primes.js
@@ -3,6 +3,8 @@ function parsegraph_PrimesWidget(app)
     this.knownPrimes = [];
     this.position = 2;
 
+    this._graph = app.graph();
+
     this.caret = new parsegraph_Caret(parsegraph_BLOCK);
     this.caret.setGlyphAtlas(app.glyphAtlas());
     this.caret.label("1");
@@ -25,10 +27,39 @@ parsegraph_PrimesWidget.prototype.step = function()
     // Check if any known prime is a multiple of the current position.
     this.caret.spawnMove('f', 'b');
     this.caret.label(this.position);
+    this.caret.node()._id = this.position;
     this.caret.push();
     this.caret.pull('u');
     this.caret.crease();
     var isPrime = true;
+
+    function addHighlights(dir) {
+        var carousel = new parsegraph_ActionCarousel(this._graph);
+        var graph = this._graph;
+        carousel.addAction("Highlight", function() {
+            var bs = parsegraph_copyStyle('s');
+            bs.backgroundColor = new parsegraph_Color(1, 1, 1, 1);
+            for(var n = this; n; n = n.nodeAt(dir)) {
+                if(n.type() === parsegraph_SLOT) {
+                    n.setBlockStyle(bs);
+                }
+            }
+            console.log("Highlighted node " + this.label());
+            graph.scheduleRepaint();
+        }, this.caret.node());
+        carousel.addAction("Unhighlight", function() {
+            var bs = parsegraph_copyStyle('s');
+            for(var n = this; n; n = n.nodeAt(dir)) {
+                if(n.type() === parsegraph_SLOT) {
+                    n.setBlockStyle(bs);
+                }
+            }
+            console.log("Unhighlighted node " + this.label());
+            graph.scheduleRepaint();
+        }, this.caret.node());
+        carousel.install(this.caret.node());
+    };
+
     for(var i = 0; i < this.knownPrimes.length; ++i) {
         var prime = this.knownPrimes[i];
         modulus = prime.calculate(this.position);
@@ -41,6 +72,7 @@ parsegraph_PrimesWidget.prototype.step = function()
         else {
             this.caret.spawnMove('u', 's');
         }
+        this.caret.node()._id = this.position + ":" + prime.frequency;
         if(i === 0) {
             this.caret.crease();
         }
@@ -49,9 +81,12 @@ parsegraph_PrimesWidget.prototype.step = function()
         // The position is prime, so output it and add it to the list.
         this.caret.spawnMove('u', 'b');
         this.caret.label(this.position);
+        this.caret.node()._id = this.position + ":" + this.position;
+        addHighlights.call(this, parsegraph_DOWNWARD);
         this.knownPrimes.push(new parsegraph_PrimesModulo(this.position));
     }
     this.caret.pop();
+    addHighlights.call(this, parsegraph_UPWARD);
 
     // Advance.
     ++(this.position);
