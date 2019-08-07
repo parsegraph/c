88b34b47bfafa7917bf13a0ce52726712002e5d2
Aaron Faanes <dafrito@gmail.com>
2019-08-07 00:22:30 -0500

    Incremental ulam

--- src/apps/ulam.js
+++ src/apps/ulam.js
@@ -1,23 +1,50 @@
-function showUlam(app, COUNT)
+function parsegraph_Ulam(app, COUNT)
 {
-    if(COUNT === undefined) {
-        COUNT = 35;
-    }
-    //COUNT = Math.min(100, COUNT);
+    this.caret = new parsegraph_Caret('u');
+    this.caret.setGlyphAtlas(app.glyphAtlas());
 
-    var ROWS = COUNT;
-
-    var total = 0;
-    for(var i = ROWS; i >= 2; --i) {
-        for(var j=1; j <= i - 1; ++j) {
-            ++total;
-        }
-        for(var j=1; j <= i - 1; ++j) {
-            ++total;
-        }
+    this.loops = COUNT;
+    if(this.loops === undefined) {
+        this.loops = 35;
     }
 
-    var MAX_CANDIDATE = total;
+    this.total = null;
+    this.count = 0;
+    this.spawnDir = parsegraph_DOWNWARD;
+    this.rowSize = 1;
+    this.position = 1;
+
+    this.knownPrimes = [];
+    this.primeMap = {};
+    this.primeMap[1] = true;
+    this.candidate = 2;
+}
+
+parsegraph_Ulam.prototype.node = function()
+{
+    return this.caret.root();
+}
+
+parsegraph_Ulam.prototype.inProgress = function()
+{
+    return this.total === null || (this.position < this.total);
+};
+
+parsegraph_Ulam.prototype.step = function()
+{
+    if(this.total === null) {
+        var total = 0;
+        for(var i = this.loops; i >= 2; --i) {
+            for(var j=1; j <= i - 1; ++j) {
+                ++total;
+            }
+            for(var j=1; j <= i - 1; ++j) {
+                ++total;
+            }
+        }
+        this.total = total;
+        return;
+    }
 
     function makeModulo(frequency) {
         var target = 0;
@@ -38,17 +65,11 @@ function showUlam(app, COUNT)
         return object;
     };
 
-    var knownPrimes = [];
-    var primeMap = {};
-    primeMap[1] = true;
-
-    var candidate = 2;
-
-    while(true) {
+    while(this.candidate <= this.total) {
         var isPrime = true;
-        for(var i = 0; i < knownPrimes.length; ++i) {
-            var prime = knownPrimes[i];
-            modulus = prime.calculate(candidate);
+        for(var i = 0; i < this.knownPrimes.length; ++i) {
+            var prime = this.knownPrimes[i];
+            modulus = prime.calculate(this.candidate);
             if(modulus == 0) {
                 // It's a multiple, so there's no chance for primality.
                 isPrime = false;
@@ -57,73 +78,54 @@ function showUlam(app, COUNT)
 
         if(isPrime) {
             // The candidate is prime, so output it and add it to the list.
-            knownPrimes.push(makeModulo(candidate));
-            primeMap[candidate] = true;
+            this.knownPrimes.push(makeModulo(this.candidate));
+            this.primeMap[this.candidate] = true;
         }
 
-        ++candidate;
-
-        if(candidate > MAX_CANDIDATE) {
-            break;
+        ++this.candidate;
+        if(this.candidate % parsegraph_NATURAL_GROUP_SIZE === 0) {
+            return;
         }
     }
 
-    // Enter
-    var spawnDir = parsegraph_FORWARD;
-    var spiralType = parsegraph_BLOCK;
-
-    var caret = new parsegraph_Caret(spiralType);
-    caret.setGlyphAtlas(app.glyphAtlas());
-    caret.fitExact();
-    caret.push();
-
-    /*for i=1, 10 do
-        spawnDir = parsegraph.turnLeft(spawnDir);
-        for j=1, i do
-            caret.pull(spawnDir);
-            caret.spawnMove(spawnDir, spiralType);
-        end;
-        spawnDir = parsegraph.turnLeft(spawnDir);
-        for j=1, i do
-            caret.pull(spawnDir);
-            caret.spawnMove(spawnDir, spiralType);
-        end;
-    end;*/
-
-    var count = 0;
-    var nextType = function() {
-        if(total - count in primeMap) {
-            ++count;
-            return parsegraph_BLOCK;
-        };
-        ++count;
-        return parsegraph_SLOT;
+    var caret = this.caret;
+    spiralType = parsegraph_BLOCK;
+    var getType = function(pos) {
+        return pos in this.primeMap ? parsegraph_BLOCK : parsegraph_SLOT;
     };
-
-    for(var i = ROWS; i >= 2; --i) {
-        spawnDir = parsegraph_turnRight(spawnDir);
-        //caret.pull(spawnDir);
-        for(var j = 1; j <= i - 1; ++j) {
-            //caret.pull(spawnDir);
-            caret.spawnMove(spawnDir, nextType());
-            if(j === 1) {
+    for(var k = 0; k < 2; ++k) {
+        for(var j = 0; j < this.loops - this.rowSize; ++j) {
+            if(j === 0) {
                 caret.crease();
             }
-            caret.label(total - count + 1);
+            var pos = 1 + this.total - this.position++;
+            caret.spawnMove(this.spawnDir, getType.call(this, pos));
+            if(j > 0) {
+                caret.fitExact();
+            }
+            else {
+                caret.fitLoose();
+            }
+            caret.label(pos);
         }
-        spawnDir = parsegraph_turnRight(spawnDir);
-        //caret.pull(spawnDir);
-        for(var j=1; j <= i-1; ++j) {
-            //caret.pull(spawnDir);
-            caret.spawnMove(spawnDir, nextType());
-            if(j === 1) {
+        this.spawnDir = parsegraph_turnRight(this.spawnDir);
+        for(var j = 0; j < this.loops - this.rowSize; ++j) {
+            if(j === 0) {
                 caret.crease();
             }
-            caret.label(total - count + 1);
+            var pos = 1 + this.total - this.position++;
+            caret.spawnMove(this.spawnDir, getType.call(this, pos));
+            caret.fitN
+            if(j > 0) {
+                caret.fitExact();
+            }
+            else {
+                caret.fitLoose();
+            }
+            caret.label(pos);
         }
+        this.spawnDir = parsegraph_turnRight(this.spawnDir);
+        ++this.rowSize;
     }
-
-    caret.pop();
-
-    return caret.root();
-}
+    return this.inProgress();
+};
