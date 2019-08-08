ff734b3c38150bce57ef18bd3e5ea1b373fb421f
Aaron Faanes <dafrito@gmail.com>
2019-08-07 22:24:31 -0500

    Refactor Ulam

--- src/apps/ulam.js
+++ src/apps/ulam.js
@@ -16,54 +16,16 @@ function parsegraph_Ulam(app, COUNT)
     this.primeMap = {};
     this.primeMap[1] = true;
     this.candidate = 2;
+
+    this.caret.fitExact();
+    this.caret.spawnMove('d', this.getType(this.position));
+    this.caret.label(this.position);
+    ++this.rowSize;
 }
-
-parsegraph_Ulam.prototype.node = function()
-{
-    return this.caret.root();
-}
-
-parsegraph_Ulam.prototype.inProgress = function()
-{
-    //console.log(this.rowSize, this.maxRows);
-    return this.rowSize < this.maxRows;
-};
-
-parsegraph_Ulam.prototype.getType = function(pos) {
-    return pos in this.primeMap ? parsegraph_BLOCK : parsegraph_SLOT;
-};
 
 parsegraph_Ulam.prototype.step = function()
 {
-    function makeModulo(frequency) {
-        var target = 0;
-
-        var object = {};
-
-        object.calculate = function(number) {
-            while(number > target) {
-                target += frequency;
-            }
-            return target - number;
-        };
-
-        object.value = function() {
-            return frequency;
-        };
-
-        return object;
-    };
-
     spiralType = parsegraph_BLOCK;
-
-    if(this.rowSize === 1) {
-        this.caret.moveToRoot();
-        this.caret.fitExact();
-        this.caret.spawnMove('d', this.getType(this.position));
-        this.caret.label(this.position);
-        ++this.rowSize;
-        return true;
-    }
 
     var reverseParentage = function(n) {
         var b = n.nodeAt(parsegraph_BACKWARD);
@@ -92,28 +54,7 @@ parsegraph_Ulam.prototype.step = function()
 
     //console.log("Position=" + this.position);
     do {
-        while(this.candidate <= this.rowSize*this.rowSize) {
-            var isPrime = true;
-            for(var i = 0; i < this.knownPrimes.length; ++i) {
-                var prime = this.knownPrimes[i];
-                modulus = prime.calculate(this.candidate);
-                if(modulus == 0) {
-                    // It's a multiple, so there's no chance for primality.
-                    isPrime = false;
-                }
-            }
-
-            if(isPrime) {
-                // The candidate is prime, so output it and add it to the list.
-                this.knownPrimes.push(makeModulo(this.candidate));
-                this.primeMap[this.candidate] = true;
-            }
-
-            ++this.candidate;
-            if(this.candidate % parsegraph_NATURAL_GROUP_SIZE === 0) {
-                return true;
-            }
-        }
+        this.computePrimes(this.rowSize*this.rowSize);
 
         this.caret.moveToRoot();
         this.caret.fitExact();
@@ -176,4 +117,60 @@ parsegraph_Ulam.prototype.step = function()
     } while(this.rowSize % 2 === 0);
 
     return this.inProgress();
+};
+
+parsegraph_Ulam.prototype.node = function()
+{
+    return this.caret.root();
+}
+
+parsegraph_Ulam.prototype.inProgress = function()
+{
+    //console.log(this.rowSize, this.maxRows);
+    return this.rowSize < this.maxRows;
+};
+
+parsegraph_Ulam.prototype.getType = function(pos) {
+    return pos in this.primeMap ? parsegraph_BLOCK : parsegraph_SLOT;
+};
+
+parsegraph_Ulam.prototype.computePrimes = function(max)
+{
+    function makeModulo(frequency) {
+        var target = 0;
+        var object = {};
+
+        object.calculate = function(number) {
+            while(number > target) {
+                target += frequency;
+            }
+            return target - number;
+        };
+
+        object.value = function() {
+            return frequency;
+        };
+
+        return object;
+    };
+
+    while(this.candidate <= max) {
+        var isPrime = true;
+        for(var i = 0; i < this.knownPrimes.length; ++i) {
+            var prime = this.knownPrimes[i];
+            modulus = prime.calculate(this.candidate);
+            if(modulus == 0) {
+                // It's a multiple, so there's no chance for primality.
+                isPrime = false;
+            }
+        }
+
+        if(isPrime) {
+            // The candidate is prime, so output it and add it to the list.
+            this.knownPrimes.push(makeModulo(this.candidate));
+            this.primeMap[this.candidate] = true;
+        }
+
+        ++this.candidate;
+    }
 };
