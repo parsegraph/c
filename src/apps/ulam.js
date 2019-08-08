ea20bbdf9c575d0caef0818cc4e31264449f6763
Aaron Faanes <dafrito@gmail.com>
2019-08-07 20:50:40 -0500

    Change how ulam spiral is drawn

--- src/apps/ulam.js
+++ src/apps/ulam.js
@@ -3,13 +3,11 @@ function parsegraph_Ulam(app, COUNT)
     this.caret = new parsegraph_Caret('u');
     this.caret.setGlyphAtlas(app.glyphAtlas());
 
-    this.loops = COUNT;
-    if(this.loops === undefined) {
-        this.loops = 35;
+    this.maxRows = COUNT;
+    if(this.maxRows === undefined) {
+        this.maxRows = 100;
     }
 
-    this.total = null;
-    this.count = 0;
     this.spawnDir = parsegraph_DOWNWARD;
     this.rowSize = 1;
     this.position = 1;
@@ -27,25 +25,16 @@ parsegraph_Ulam.prototype.node = function()
 
 parsegraph_Ulam.prototype.inProgress = function()
 {
-    return this.total === null || (this.position < this.total);
+    //console.log(this.rowSize, this.maxRows);
+    return this.rowSize < this.maxRows;
+};
+
+parsegraph_Ulam.prototype.getType = function(pos) {
+    return pos in this.primeMap ? parsegraph_BLOCK : parsegraph_SLOT;
 };
 
 parsegraph_Ulam.prototype.step = function()
 {
-    if(this.total === null) {
-        var total = 0;
-        for(var i = this.loops; i >= 2; --i) {
-            for(var j=1; j <= i - 1; ++j) {
-                ++total;
-            }
-            for(var j=1; j <= i - 1; ++j) {
-                ++total;
-            }
-        }
-        this.total = total;
-        return;
-    }
-
     function makeModulo(frequency) {
         var target = 0;
 
@@ -65,67 +54,126 @@ parsegraph_Ulam.prototype.step = function()
         return object;
     };
 
-    while(this.candidate <= this.total) {
-        var isPrime = true;
-        for(var i = 0; i < this.knownPrimes.length; ++i) {
-            var prime = this.knownPrimes[i];
-            modulus = prime.calculate(this.candidate);
-            if(modulus == 0) {
-                // It's a multiple, so there's no chance for primality.
-                isPrime = false;
-            }
-        }
-
-        if(isPrime) {
-            // The candidate is prime, so output it and add it to the list.
-            this.knownPrimes.push(makeModulo(this.candidate));
-            this.primeMap[this.candidate] = true;
-        }
-
-        ++this.candidate;
-        if(this.candidate % parsegraph_NATURAL_GROUP_SIZE === 0) {
-            return;
-        }
-    }
-
-    var caret = this.caret;
     spiralType = parsegraph_BLOCK;
-    var getType = function(pos) {
-        return pos in this.primeMap ? parsegraph_BLOCK : parsegraph_SLOT;
-    };
-    for(var k = 0; k < 2; ++k) {
-        for(var j = 0; j < this.loops - this.rowSize; ++j) {
-            if(j === 0) {
-                caret.crease();
-            }
-            var pos = 1 + this.total - this.position++;
-            caret.spawnMove(this.spawnDir, getType.call(this, pos));
-            if(j > 0) {
-                caret.fitExact();
-            }
-            else {
-                caret.fitLoose();
-            }
-            caret.label(pos);
-        }
-        this.spawnDir = parsegraph_turnRight(this.spawnDir);
-        for(var j = 0; j < this.loops - this.rowSize; ++j) {
-            if(j === 0) {
-                caret.crease();
-            }
-            var pos = 1 + this.total - this.position++;
-            caret.spawnMove(this.spawnDir, getType.call(this, pos));
-            caret.fitN
-            if(j > 0) {
-                caret.fitExact();
-            }
-            else {
-                caret.fitLoose();
-            }
-            caret.label(pos);
-        }
-        this.spawnDir = parsegraph_turnRight(this.spawnDir);
+
+    if(this.rowSize === 1) {
+        this.caret.moveToRoot();
+        this.caret.fitExact();
+        this.caret.spawnMove('d', this.getType(this.position));
+        this.caret.label(this.position);
         ++this.rowSize;
+        return true;
     }
+
+    var reverseParentage = function(n) {
+        var b = n.nodeAt(parsegraph_BACKWARD);
+        while(b) {
+            b.disconnectNode();
+            b.connectNode(parsegraph_FORWARD, n);
+            n = b;
+            b = b.nodeAt(parsegraph_BACKWARD);
+        }
+        b = n.nodeAt(parsegraph_DOWNWARD);
+        while(b) {
+            n.disconnectNode(parsegraph_DOWNWARD);
+            b.connectNode(parsegraph_UPWARD, n);
+            n = b;
+            b = b.nodeAt(parsegraph_DOWNWARD);
+        }
+        b = n.nodeAt(parsegraph_FORWARD);
+        while(b) {
+            n.disconnectNode(parsegraph_FORWARD);
+            b.connectNode(parsegraph_BACKWARD, n);
+            n = b;
+            b = b.nodeAt(parsegraph_FORWARD);
+        }
+        return n;
+    };
+
+    //console.log("Position=" + this.position);
+    do {
+        while(this.candidate <= this.rowSize*this.rowSize) {
+            var isPrime = true;
+            for(var i = 0; i < this.knownPrimes.length; ++i) {
+                var prime = this.knownPrimes[i];
+                modulus = prime.calculate(this.candidate);
+                if(modulus == 0) {
+                    // It's a multiple, so there's no chance for primality.
+                    isPrime = false;
+                }
+            }
+
+            if(isPrime) {
+                // The candidate is prime, so output it and add it to the list.
+                this.knownPrimes.push(makeModulo(this.candidate));
+                this.primeMap[this.candidate] = true;
+            }
+
+            ++this.candidate;
+            if(this.candidate % parsegraph_NATURAL_GROUP_SIZE === 0) {
+                return true;
+            }
+        }
+
+        this.caret.moveToRoot();
+        this.caret.fitExact();
+        if(this.rowSize % 2 === 0) {
+            //console.log("Even row " + this.rowSize);
+            var node = this.caret.disconnect('d');
+            var caret = new parsegraph_Caret();
+            caret.fitExact();
+            caret.setGlyphAtlas(this.caret.glyphAtlas());
+            var cornerPosition = this.rowSize*this.rowSize - (this.rowSize-1);
+            caret.replace(this.getType(cornerPosition));
+            //console.log("cornerPosition=" + cornerPosition);
+            caret.label(cornerPosition);
+            this.corner = caret.node();
+            caret.push();
+            for(var i = cornerPosition + 1; i <= this.rowSize*this.rowSize; ++i) {
+                caret.spawnMove('b', this.getType(i));
+                caret.label(i);
+                this.position = i;
+            }
+            this.last = caret.node();
+            caret.pop();
+            caret.push();
+            caret.pull('d');
+            for(var i = 0; i < this.rowSize-1; ++i) {
+                var pos = cornerPosition - 1 - i;
+                caret.spawnMove('d', this.getType(pos));
+                caret.label(pos);
+            }
+            caret.connect('b', reverseParentage(node));
+            caret.pop();
+            this.caret.connect('d', this.corner);
+            this.caret.fitLoose();
+            this.caret.crease();
+            //console.log(this.caret.node());
+            ++this.rowSize;
+        }
+        else {
+            //console.log("Odd row " + this.rowSize);
+            var caret = new parsegraph_Caret(this.last, this.caret.glyphAtlas());
+            var pos = this.rowSize*this.rowSize+1;
+            ++this.position;
+            caret.spawnMove('b', this.getType(this.position));
+            caret.label(this.position);
+            ++this.rowSize;
+            for(var i = pos+1; i <= this.rowSize*this.rowSize - this.rowSize; ++i) {
+                ++this.position;
+                caret.spawnMove('d', this.getType(this.position));
+                caret.label(this.position);
+            }
+            pos = i;
+            for(var i = 0; i < this.rowSize-2; ++i) {
+                ++this.position;
+                caret.spawnMove('f', this.getType(this.position));
+                caret.label(this.position);
+            }
+            caret.crease();
+            this.last = caret.node();
+        }
+    } while(this.rowSize % 2 === 0);
+
     return this.inProgress();
 };
