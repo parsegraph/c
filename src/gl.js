932a39d69d289db4ed829e922fe0c0115a1caab7
Aaron Faanes <dafrito@gmail.com>
2019-08-24 23:02:47 -0500

    parsegraph_glErrorString

--- src/gl.js
+++ src/gl.js
@@ -129,6 +129,23 @@ function createProgram(gl, vertexShader, fragmentShader) {
   return program;
 };
 
+function parsegraph_glErrorString(gl, err)
+{
+    if(arguments.length < 2) {
+        throw new Error("A GL context must be provided with the error");
+    }
+    switch(0 + err) {
+    case gl.NO_ERROR: return "NO_ERROR";
+    case gl.INVALID_ENUM: return "INVALID_ENUM";
+    case gl.INVALID_VALUE: return "INVALID_VALUE";
+    case gl.INVALID_OPERATION: return "INVALID_OPERATION";
+    case gl.INVALID_FRAMEBUFFER_OPERATION: return "INVALID_FRAMEBUFFER_OPERATION";
+    case gl.OUT_OF_MEMORY: return "OUT_OF_MEMORY";
+    case gl.CONTEXT_LOST_WEBGL: return "CONTEXT_LOST_WEBGL";
+    default: return err;
+    }
+}
+
 function parsegraph_compileProgram(gl, shaders, shaderName, vertexShader, fragShader)
 {
     if(shaders[shaderName]) {
