f00049ce083bdc7a55924ff269761f3f793ba8ae
Aaron Faanes <dafrito@gmail.com>
2019-10-17 17:35:39 -0500

    parsegraph_Method

---
+++ src/function.js
@@ -0,0 +1,15 @@
+function parsegraph_Method(func, funcThisArg)
+{
+    this._func = func;
+    this._funcThisArg = funcThisArg;
+}
+
+parsegraph_Method.prototype.call = function()
+{
+    return this._func.apply(this._funcThisArg, arguments);
+};
+
+parsegraph_Method.prototype.apply = function(args)
+{
+    return this._func.apply(this._funcThisArg, args);
+};
