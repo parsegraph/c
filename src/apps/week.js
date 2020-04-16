ff11190ea2da432f807a80c21bdf707d1d7dc170
Aaron Faanes <dafrito@gmail.com>
2020-04-16 10:22:35 -0500

    Extract week

---
+++ src/apps/week.js
@@ -0,0 +1,25 @@
+function createWeek(belt, world)
+{
+    var car = new parsegraph_Caret(parsegraph_BUD);
+    [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ].forEach(function(weekday, i) {
+        if(i !== 0) {
+            car.spawnMove('f', 'bu');
+        }
+
+        car.connect('d', createDay(i, null, null, null, null, function() {
+            belt.scheduleUpdate();
+            world.scheduleRepaint();
+        }, this));
+        car.pull('d');
+    }, this);
+    return car.root();
+}
+
