d618ba9215aae035d3212fcf2c736edbdea31daf
Aaron Faanes <dafrito@gmail.com>
2020-08-25 00:12:38 -0500

    Include config files in autodeploy

--- autodeploy.sh
+++ autodeploy.sh
@@ -2,5 +2,5 @@
 
 while true; do
     ./deploy.sh
-    inotifywait -e modify -r deploy.sh src www www/doc www/sitemap --format '%w %e' | read file event;
+    inotifywait -e modify -r .babelrc webpack.config.js package.json autodeploy.sh deploy.sh src www www/doc www/sitemap --format '%w %e' | read file event;
 done
