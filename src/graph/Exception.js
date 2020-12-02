a48af2992d97d29b542144d7f387ab55704f5924
Tracy Thorn <ardiosomega@gmail.com>
2020-12-02 10:11:46 -0600

    Exception.js Eslint fixes

--- src/graph/Exception.js
+++ src/graph/Exception.js
@@ -1,66 +1,67 @@
-export const parsegraph_NULL_STATUS = 0;
-export const parsegraph_OK = 1;
-export const parsegraph_BAD_STATUS = 2;
-export const parsegraph_NO_NODE_FOUND = 3;
-export const parsegraph_ALREADY_OCCUPIED = 4;
-export const parsegraph_BAD_NODE_DIRECTION = 5;
-export const parsegraph_BAD_NODE_CONTENT = 6;
-export const parsegraph_BAD_AXIS = 7;
-export const parsegraph_BAD_LAYOUT_STATE = 8;
-export const parsegraph_BAD_NODE_ALIGNMENT = 9;
-export const parsegraph_CANNOT_AFFECT_PARENT = 10;
-export const parsegraph_OFFSET_IS_NEGATIVE = 11;
-export const parsegraph_NODE_IS_ROOT = 12;
-export const parsegraph_BAD_LAYOUT_PREFERENCE = 13;
-export const parsegraph_BAD_AXIS_OVERLAP = 14;
-export const parsegraph_BAD_NODE_TYPE = 15;
-export const parsegraph_BAD_NODE_FIT = 16;
+export const NULL_STATUS = 0;
+export const OK = 1;
+export const BAD_STATUS = 2;
+export const NO_NODE_FOUND = 3;
+export const ALREADY_OCCUPIED = 4;
+export const BAD_NODE_DIRECTION = 5;
+export const BAD_NODE_CONTENT = 6;
+export const BAD_AXIS = 7;
+export const BAD_LAYOUT_STATE = 8;
+export const BAD_NODE_ALIGNMENT = 9;
+export const CANNOT_AFFECT_PARENT = 10;
+export const OFFSET_IS_NEGATIVE = 11;
+export const NODE_IS_ROOT = 12;
+export const BAD_LAYOUT_PREFERENCE = 13;
+export const BAD_AXIS_OVERLAP = 14;
+export const BAD_NODE_TYPE = 15;
+export const BAD_NODE_FIT = 16;
+/* eslint-disable require-jsdoc */
 
-export function parsegraph_nameStatus(given) {
+export function nameStatus(given) {
   switch (given) {
-    case parsegraph_NULL_STATUS:
+    case NULL_STATUS:
       return 'NULL_STATUS';
-    case parsegraph_OK:
+    case OK:
       return 'OK';
-    case parsegraph_NO_NODE_FOUND:
+    case NO_NODE_FOUND:
       return 'NO_NODE_FOUND';
-    case parsegraph_ALREADY_OCCUPIED:
+    case ALREADY_OCCUPIED:
       return 'ALREADY_OCCUPIED';
-    case parsegraph_BAD_NODE_DIRECTION:
+    case BAD_NODE_DIRECTION:
       return 'BAD_NODE_DIRECTION';
-    case parsegraph_BAD_NODE_CONTENT:
+    case BAD_NODE_CONTENT:
       return 'BAD_NODE_CONTENT';
-    case parsegraph_BAD_AXIS:
+    case BAD_AXIS:
       return 'BAD_AXIS';
-    case parsegraph_BAD_LAYOUT_STATE:
+    case BAD_LAYOUT_STATE:
       return 'BAD_LAYOUT_STATE';
-    case parsegraph_BAD_NODE_ALIGNMENT:
+    case BAD_NODE_ALIGNMENT:
       return 'BAD_NODE_ALIGNMENT';
-    case parsegraph_NODE_IS_ROOT:
+    case NODE_IS_ROOT:
       return 'NODE_IS_ROOT';
-    case parsegraph_BAD_STATUS:
+    case BAD_STATUS:
       return 'BAD_STATUS';
-    case parsegraph_CANNOT_AFFECT_PARENT:
+    case CANNOT_AFFECT_PARENT:
       return 'CANNOT_AFFECT_PARENT';
-    case parsegraph_OFFSET_IS_NEGATIVE:
+    case OFFSET_IS_NEGATIVE:
       return 'OFFSET_IS_NEGATIVE';
-    case parsegraph_BAD_LAYOUT_PREFERENCE:
+    case BAD_LAYOUT_PREFERENCE:
       return 'BAD_LAYOUT_PREFERENCE';
-    case parsegraph_BAD_AXIS_OVERLAP:
+    case BAD_AXIS_OVERLAP:
       return 'BAD_AXIS_OVERLAP';
-    case parsegraph_BAD_NODE_TYPE:
+    case BAD_NODE_TYPE:
       return 'BAD_NODE_TYPE';
-    case parsegraph_BAD_NODE_FIT:
+    case BAD_NODE_FIT:
       return 'BAD_NODE_FIT';
   }
-  throw parsegraph_createException(parsegraph_BAD_STATUS, given);
+  throw createException(BAD_STATUS, given);
 }
 
-export function parsegraph_createException(exceptionCode) {
-  if (arguments.length > 1) {
+export function createException(exceptionCode, ...args) {
+  if (args.length > 1) {
     return new Error(
-        parsegraph_nameStatus(exceptionCode) + '\nArgument: ' + arguments[1],
+        nameStatus(exceptionCode) + '\nArgument: ' + args[1],
     );
   }
-  return new Error(parsegraph_nameStatus(exceptionCode));
+  return new Error(nameStatus(exceptionCode));
 }
