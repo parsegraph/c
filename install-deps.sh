5583b9d1a25f835ea85588c8a1e04e94804e08c4
Aaron Faanes <dafrito@gmail.com>
2020-12-04 23:13:51 -0600

    Add missing dep to install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dnf install make httpd-devel apr-devel apr-util-devel autoconf automake libtool pango-devel libdrm-devel mesa-libgbm-devel mesa-libEGL-devel mesa-libGL-devel libudev-devel libinput-devel libevdev-devel ncurses-devel libicu-devel ncurses-devel libicu-devel openssl-devel qt5-qtbase-devel harfbuzz-devel libcurl-devel libgbm-devel mesa-libOSMesa-devel
+dnf install make httpd-devel apr-devel apr-util-devel autoconf automake libtool pango-devel libdrm-devel mesa-libgbm-devel mesa-libEGL-devel mesa-libGL-devel libudev-devel libinput-devel libevdev-devel ncurses-devel libicu-devel ncurses-devel libicu-devel openssl-devel qt5-qtbase-devel harfbuzz-devel libcurl-devel libgbm-devel mesa-libOSMesa-devel mesa-libGLU-devel
