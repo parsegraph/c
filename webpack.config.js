05aba02a14bdd9fa86656f22f3768d601e2e2520
Aaron Faanes <dafrito@gmail.com>
2020-12-05 23:28:30 -0600

    Also use node_modules to resolve webpack

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   },
   resolve: {
     extensions: [".js", ".ts", ".tsx", ".glsl"],
-    modules: [path.resolve(__dirname, "src")],
+    modules: [path.resolve(__dirname, "src"), "node_modules"]
   },
   mode: "development",
   devtool: "inline-source-map",
